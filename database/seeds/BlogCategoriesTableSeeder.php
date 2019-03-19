6949e1f0f3422e7d7b82e75b011c90a1f12d675e
Nikola-Y <mukola787898@gmail.com>
2019-03-19 09:32:50 +0200

    Version 36 | DataBase | Laravel 5.8 18.03.2019 @master

---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder{
+
+    public function run(){
+        $categories = [];
+
+        $cName        = 'Без категории';
+        $categories[] = [
+            'title'     => $cName,
+            'slug'      => str_slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for($i = 2;$i <= 11;$i++){
+            $cName     = 'Категория #' . $i;
+            $parent_id = ($i > 4) ? rand(1, 4) : 1;
+
+            $categories[] = [
+                'title'     => $cName,
+                'slug'      => str_slug($cName),
+                'parent_id' => $parent_id,
+            ];
+        }
+        DB::table('blog_categories')->insert($categories);
+    }
+}
