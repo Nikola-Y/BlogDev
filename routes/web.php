f6591abf48e4828eee0745ad3a55ec5d130b7743
Nikola-Y <mukola787898@gmail.com>
2019-03-19 17:19:37 +0200

    Version 37 Laravel 5.8 19.03.2019 @master

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,36 +11,10 @@
 |
 */
 
-Route::get('/',
-    function(){
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::group(['namespace' => 'Blog', 'prefix' => 'blog'],
-    function(){
-        Route::resource('posts', 'PostController')->names('blog.posts');
-    });
-
-//>Админка блога
-$groupData = [
-    'namespace' => 'Blog\Admin',
-    'prefix' => 'admin/blog',
-];
-
-Route::group($groupData,
-    function(){
-        //BlogCategory
-        $methods = ['index', 'edit', 'update', 'create', 'store',];
-        Route::resource('categories', 'CategoryController')->only($methods)
-             ->names('blog.admin.categories');
-
-        //BlogPost
-        Route::resource('posts', 'PostController')->except(['show'])
-             ->names('blog.admin.posts');
-    });
-
-//<
-//Route::resource('rest', 'RestTestController')->names('restTest');
-
